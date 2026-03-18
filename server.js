cabe666b1ddee60abc31daf6134e27b4b45c98b7
yuta578 <guevarajhoan47@gmail.com>
2026-03-18 01:41:21 -0500

    Express server test 2

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import express from "express";
-
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Servidor funcionando - test 1");
 });
 
-app.listen(3000, () => {
-  console.log("Server running - test 1");
+app.listen(PORT, () => {
+  console.log("Server running");
 });
